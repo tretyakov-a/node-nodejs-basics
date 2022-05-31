bdbfd68082df54839cdf996579fe1b9a01af5167
Alexander Tretyakov <tpetyakov@gmail.com>
2022-05-31 11:08:42 +0300

    feat: add worker threads subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
-export const performCalculations = async () => {
-    // Write your code here
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const toWorker = (n) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: { n } });
+    worker.on('message', (data) => resolve(data));
+    worker.on('error', (err) => reject(err));
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    });
+  });
 };
+
+const toCorrectResult = ({ status, value, reason }) => {
+  const newStatus = status === 'fulfilled' ? 'resolved' : 'error';
+  const data = value === undefined ? null : value;
+  return { status: newStatus, data };
+};
+
+export const performCalculations = async () => {
+  const cpusNumber = os.cpus().length;
+  const startValue = 10;
+  const data = Array.from({ length: cpusNumber }, (_, i) => startValue + i);
+  try {
+    const results = await Promise.allSettled(data.map(toWorker));
+    return results.map(toCorrectResult);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+performCalculations()
+  .then((data) => console.log(data));
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,20 @@
-// n should be received from main thread
+import { workerData, parentPort } from 'worker_threads';
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(workerData.n);
+  const rndNumber = Math.random();
+
+  // testing errors and process stops
+  if (rndNumber < 0.1) {
+    throw new Error('Error in worker');
+  }
+  if (rndNumber > 0.1 && rndNumber < 0.2) {
+    process.exitCode = 1;
+  } else {
+    parentPort.postMessage(result);
+  }
 };
+
+sendResult();
