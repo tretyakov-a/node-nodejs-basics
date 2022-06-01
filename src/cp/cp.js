6b39a6681638425852bcff8ac1ecb8064d2c3c88
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-01 09:31:41 +0300

    feat: add child process subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { fork } from 'child_process';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const DIR_NAME = 'files';
+const FILE_NAME = 'script.js';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptSrc = path.join(__dirname, DIR_NAME, FILE_NAME);
+
+  const child = fork(scriptSrc, args, { stdio: 'inherit' });
+
+  child.on('error', (err) => {
+    console.error(err);
+  });
+
+  child.on('close', (code) => {
+    console.log(`Child process closed with code ${code}. `);
+  });
 };
+
+spawnChildProcess(['arg1', 'arg2']);
--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -1,11 +1,12 @@
-const arguments = process.argv.slice(2);
+const args = process.argv.slice(2);
 
-console.log(`Total number of arguments is ${arguments.length}`);
-console.log(`Arguments: ${JSON.stringify(arguments)}`);
+console.log(`Total number of arguments is ${args.length}`);
+console.log(`Arguments: ${JSON.stringify(args)}`);
 
 const echoInput = (chunk) => {
     const chunkStringified = chunk.toString();
     if (chunkStringified.includes('CLOSE')) process.exit(0);
+    if (chunkStringified.includes('ERROR')) throw new Error('Error in child process!');
     process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
 };
 
