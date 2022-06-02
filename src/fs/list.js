3ad2dab953ca6a63a086d445fbe7a3f4d85d3703
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-02 15:42:47 +0300

    fix: fix fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -7,26 +7,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const ERROR_MSG = 'FS operation failed';
 const DIR_NAME = 'files';
 
-async function getFilesStats(source) {
-  const readDir = async (dirPath, parentDir = '') => {
-    const files = await fsPromises.readdir(dirPath, { withFileTypes: true });
-    const fileNames = await Promise.all(files.map((file) => {
-      const filePath = path.join(dirPath, file.name);
-      const fileName = `${parentDir}${parentDir && '/'}${file.name}`;
-      return file.isDirectory()
-        ? readDir(filePath, fileName)
-        : [ fileName ];
-    }));
-    return fileNames.reduce((acc, el) => [...acc, ...el], []);
-  };
-  return await readDir(source);
-}
-
 export const list = async () => {
   const source = path.join(__dirname, DIR_NAME); 
   try {
-    const filesStats = await getFilesStats(source);
-    console.log(filesStats.join('\n'));
+    const filesStats = await fsPromises.readdir(source, { withFileTypes: true });
+    console.log(filesStats.map((file) => file.name).join('\n'));
   } catch (err) {
     if (err.code === 'ENOENT') {
       throw new Error(ERROR_MSG);
