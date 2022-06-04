c0a67716038c5a2e86b21c57c866faed56f89c25
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-04 10:32:38 +0300

    feat: add basic validation to cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,17 +1,29 @@
 const ARGS_SEP = ', ';
-const NAME_VALUE_SEP = ' is ';
+const ARG_PREFIX = '--';
+
+const propNameRegExp = new RegExp(`^${ARG_PREFIX}[\\w]{1,}[\\w]*$`);
+const isValidPropName = (name) => propNameRegExp.test(name);
 
 export const parseArgs = () => {
   const args = process.argv.slice(2);
   const argsAsNameValue = [];
   for (let i = 0; i < args.length; i += 2) {
-    const nameValue = args
-      .slice(i, i + 2)
-      .join(NAME_VALUE_SEP)
-      .slice(2);
-    argsAsNameValue.push(nameValue);
-  }
+    const propName = args[i];
+    if (isValidPropName(propName)) {
+      const value = args[i + 1];
+      if (!value || isValidPropName(value)) {
+        throw new Error(`Property '${propName}' has now value`);
+      }
+      argsAsNameValue.push(`${propName.slice(ARG_PREFIX.length)} is ${value}`);
+    } else {
+      throw new Error(`Argument name '${propName}' invalid`);
+    }
+  };
   console.log(argsAsNameValue.join(ARGS_SEP));
 };
 
-parseArgs();
+try {
+  parseArgs();
+} catch (err) {
+  console.error(err.message);
+}
