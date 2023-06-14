9e22d151ed7af25f7fc62c612a09ecaa904b70d5
Alexander Tretyakov <tpetyakov@gmail.com>
2023-06-14 10:12:30 +0300

    refactor: remove files

--- src/modules/test.js
+++
@@ -1,7 +0,0 @@
-import { unknownObject, createMyServer } from './cjsToEsm.mjs';
-
-console.log(unknownObject);
-
-const PORT = 8080;
-console.log(`Server on localhost:${PORT}`);
-createMyServer.listen(PORT);
